ae3cdfd39d9cd3538a8822791c249296d28f616a
Yesid Marin <yesmagodev07@gmail.com>
2025-11-03 13:24:46 -0500

    Yesmagodev: Add 404 page - Change Social Preview Image

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import preact from '@astrojs/preact';
 // https://astro.build/config
 export default defineConfig({
   integrations: [preact()],
-  site: 'https://la-url-de-tu-netlify.netlify.app'
+  site: 'https://yesmagodev.netlify.app/'
 });
